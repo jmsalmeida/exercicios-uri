4ee7344a6b8b7604f73be4fee5e7bdbf0bed92ce
James Almeida <James Almeida>
2020-12-16 21:28:54 -0300

    Refactor 1018 bank notes

--- beginner/1018.rb
+++ beginner/1018.rb
@@ -1,26 +1,23 @@
-# Consider timelimit
-# Como consertar pra inserir os 0s
-# Outro raciocinio que pode ser usado pra resolver o mesmo problema
-
 bank_notes = [1, 2, 5, 10, 20, 50, 100]
 
-while a = gets
-  a = a.chomp.to_i
-  aggregator = 0
+while value = gets
+  value = value.chomp.to_i
   notes_to_print = {}
 
+  value_to_descount = value
   bank_notes.reverse_each do |note|
     notes_to_print[note] = 0
 
-    while aggregator + note <= a
-      notes_to_print[note] = notes_to_print[note] + 1
-
-      aggregator += note
+    numero_notas = value_to_descount / note
+    if numero_notas > 0
+      notes_to_print[note] = numero_notas
+      value_to_descount -= (note * notes_to_print[note])
     end
   end
 
-  puts aggregator
+  puts value
   notes_to_print.each do |note, counter|
     puts "#{counter} nota(s) de R$ %d,00" % note
   end
 end
+
